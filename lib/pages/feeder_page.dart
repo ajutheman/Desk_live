7fb0b3d947595fb600304a0ef3ae689c2989821a
ajmalm <ajmalmundeth1324@gmail.com>
2023-09-20 13:14:13 +0530

    added feeder_page.dart

---
+++ lib/pages/feeder_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeederPage extends StatefulWidget {
+  const FeederPage({super.key});
+
+  @override
+  State<FeederPage> createState() => _FeederPageState();
+}
+
+class _FeederPageState extends State<FeederPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
